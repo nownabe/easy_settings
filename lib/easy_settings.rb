52c9929db063503fe7ca4ab18fe7e8cde9ed2049
nownabe <nownabe@gmail.com>
2015-03-06 02:18:14 +0900

    load yaml

--- lib/easy_settings.rb
+++ lib/easy_settings.rb
@@ -1,9 +1,13 @@
 require "hashie"
+require "yaml"
+require "erb"
 
 class EasySettings < Hashie::Mash
   VERSION = "0.0.1"
 
   class << self
+    attr_accessor :source_hash, :source_file, :namespace
+
     def method_missing(method_name, *args, &blk)
       p method_name
       p args
@@ -11,13 +15,45 @@ class EasySettings < Hashie::Mash
     end
 
     def instance
-      @mash ||= new
+      @instance ||= new(source_hash || source_file, nil, namespace)
     end
+
+    def load!
+      instance
+      true
+    end
+
+    def reload!
+      @instance = nil
+      load!
+    end
+  end
+
+  def initialize(source = nil, default = nil, namespace = nil, &blk)
+    source = load_source_file(source) if source.nil? or source.is_a?(String)
+    source = source[namespace] if namespace
+    super(source, default)
   end
 
   def assign_property(name, value)
     super
     self[name]
+  end
+
+  def find_file(file)
+    return file if file and FileTest.exist?(file)
+    ["settings.yml", "config/settings.yml"].each do |f|
+      path = File.expand_path(f, Dir.pwd)
+      return path if FileTest.exist?(path)
+    end
+    nil
+  end
+
+  def load_source_file(source)
+    file = find_file(source)
+    return nil unless file # = find_file(source)
+    content = File.read(file)
+    content.empty? ? {} : YAML.load(ERB.new(content).result).to_hash
   end
 
   def method_missing(method_name, *args, &blk)
