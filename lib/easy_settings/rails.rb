8f634766f4af9c9392a92423020f7d42bea3daa7
nownabe <nownabe@gmail.com>
2015-10-05 19:30:47 +0900

    :art: Use Railtie in Rails integration

--- lib/easy_settings/rails.rb
+++ lib/easy_settings/rails.rb
@@ -1,1 +1,8 @@
-EasySettings.namespace = Rails.env
+class EasySettings
+  class Railtie < ::Rails::Railtie
+    initializer "easy_settings" do |app|
+      EasySettings.namespace   ||= Rails.env
+      EasySettings.source_file ||= Rails.root.join("config/settings.yml").to_s
+    end
+  end
+end
