84572bb58b8b9c5fe33a6c57b158097e32c79e3e
nownabe <nownabe@gmail.com>
2015-10-05 19:33:56 +0900

    :white_check_mark: Add test of source_file for rails integration

--- spec/easy_settings_test/spec/easy_settings_spec.rb
+++ spec/easy_settings_test/spec/easy_settings_spec.rb
@@ -2,8 +2,9 @@ require File.expand_path("../rails_helper", __FILE__)
 
 describe EasySettings do
   subject { described_class }
-  its(:namespace) { is_expected.to eq Rails.env }
-  its(:app_name)  { is_expected.to eq "Test App for EasySettings" }
-  its(:endpoint)  { is_expected.to eq "https://endpoint-for-test" }
-  its(:apikey)    { is_expected.to eq "EASYSETTINGS" }
+  its(:namespace)   { is_expected.to eq Rails.env }
+  its(:source_file) { is_expected.to eq Rails.root.join("config/settings.yml").to_s }
+  its(:app_name)    { is_expected.to eq "Test App for EasySettings" }
+  its(:endpoint)    { is_expected.to eq "https://endpoint-for-test" }
+  its(:apikey)      { is_expected.to eq "EASYSETTINGS" }
 end
